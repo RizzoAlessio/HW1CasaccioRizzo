0df5389cdfb42ce158ac90beed3f5f2b1041b642
AgrippinoC <128165350+AgrippinoC@users.noreply.github.com>
2025-11-29 10:37:19 +0100

    Delete nuovo directory

--- nuovo/DataCollector/grpc_client.py
+++
@@ -1,9 +0,0 @@
-import grpc
-import auth_pb2, auth_pb2_grpc
-
-def check_user(email):
-    channel = grpc.insecure_channel("service_b:6000")
-    stub = auth_pb2_grpc.AuthServiceStub(channel)
-
-    response = stub.CheckUser(auth_pb2.UserRequest(email=email))
-    return response.exists
